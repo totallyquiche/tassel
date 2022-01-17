49d92f467976323127628aa842f28b8c891d1c37
Brian Dady <bndady@gmail.com>
2022-01-16 20:42:46 -0600

    Adds non-functional cloud save icon

--- tassel/resources/views/home.blade.php
+++ tassel/resources/views/home.blade.php
@@ -32,6 +32,11 @@
                 <button class="ql-list" value="bullet"></button>
                 <button class="ql-link"></button>
                 <button class="ql-clean"></button>
+                <button id="ql-save-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="lightslategray">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+                    </svg>
+                </button>
             </div>
 
             <div class="text-slate-600 mt-4 mb-12 bg-slate-200 h-96 overflow-auto">
