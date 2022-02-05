0b4569e1e2282e5554c6ad09a8cce155133b110c
Brian Dady <bndady@gmail.com>
2022-02-05 15:38:02 -0600

    Adds overlay toggle icon

--- resources/views/prompt.blade.php
+++ resources/views/prompt.blade.php
@@ -27,6 +27,11 @@
         <button class="ql-list" value="bullet"></button>
         <button class="ql-link"></button>
         <button class="ql-clean"></button>
+        <button id="toggle-overlay-button">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="lightslategray">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
+            </svg>
+        </button>
         @livewire('save-draft-button')
         @livewire('delete-drafts-button')
     </div>
@@ -108,18 +113,26 @@
                 }
             });
 
-            document.addEventListener('keydown', (event) => {
-                if (event.key === 'Escape') {
-                    const overlay = document.querySelector('#overlay');
-                    const top = overlay.style.top;
+            const toggleOverlay = () => {
+                const overlay = document.querySelector('#overlay');
+                const top = overlay.style.top;
 
-                    overlay.style.top = ((!top || top === '-100%') ? 0 : '-100%');
+                overlay.style.top = ((!top || top === '-100%') ? 0 : '-100%');
+            };
+
+            document.addEventListener('keydown', event => {
+                if (event.key && event.key === 'Escape') {
+                    toggleOverlay();
                 }
             });
 
-            document.querySelector('#overlay').addEventListener('click', function(event) {
-                this.style.top = '-100%';
-            });
+            document.querySelector('#overlay').addEventListener('click',
+                () => toggleOverlay()
+            );
+
+            document.querySelector('#toggle-overlay-button').addEventListener('click',
+                () => toggleOverlay()
+            );
         </script>
     @endpush
 </x-app-layout>
