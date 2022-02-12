140ce2695b2951fbe452af064798b440c8b54096
Brian Dady <bndady@gmail.com>
2022-02-12 13:55:39 -0600

    Updates margin on draft delete button on drafts page

--- resources/views/livewire/draft-cards.blade.php
+++ resources/views/livewire/draft-cards.blade.php
@@ -8,7 +8,10 @@
                     <div class="flex mb-2">
                         <a class="underline text-blue-200 hover:text-blue-400" href="{{ route('prompt', ['id' => $draft->Prompt->id]) }}">{{ date('m/d/y \a\t g:i a', strtotime($draft->updated_at)) }}</a>
                         &nbsp;&nbsp;
-                        <button onclick="confirm('Are you sure you want to delete this draft?') || event.stopImmediatePropagation()" wire:click="deleteDraft({{ $draft->id }})" class="font-bold rounded-full bg-cyan-600 text-blue-200 hover:text-blue-600 flex items-center justify-center font-mono h-4 w-4">x</button>
+                        <button
+                            onclick="confirm('Are you sure you want to delete this draft?') || event.stopImmediatePropagation()"
+                            wire:click="deleteDraft({{ $draft->id }})"
+                            class="font-bold rounded-full bg-cyan-600 text-blue-200 hover:text-blue-600 flex items-center justify-center font-mono h-4 w-4 mt-0.5">x</button>
                     </div>
                     <span class="italic font-bold">Prompt:</span> {{ $draft->Prompt->text }}
                 </div>
