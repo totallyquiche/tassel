db453212cb811298938c5312d3a1f4cb51a2986e
Brian Dady <bndady@gmail.com>
2022-02-12 13:05:43 -0600

    Corrects logo display on overlay

--- resources/views/prompt.blade.php
+++ resources/views/prompt.blade.php
@@ -5,8 +5,8 @@
 
     <x-slot name="header">Write Something.</x-slot>
 
-    <div id="overlay" class="fixed top-[-100%] left-0 right-0 bottom-0 w-full h-full z-50 cursor-pointer bg-cyan-900 opacity-[0.95]" style="transition: top 0.20s ease-in-out;');">
-        <img class="max-h-full mx-auto" src="{{ asset('storage/images/logo-white.png') }}" />
+    <div id="overlay" class="flex items-center justify-center fixed top-[-100%] left-0 right-0 bottom-0 w-full h-full z-50 cursor-pointer bg-cyan-900 opacity-[0.95]" style="transition: top 0.20s ease-in-out;');">
+        <img class="max-h-[25%]" src="{{ asset('storage/images/logo-white.png') }}" />
     </div>
 
     <span class="text-slate-300">
